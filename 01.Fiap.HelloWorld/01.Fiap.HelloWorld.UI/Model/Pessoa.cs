de5f8330909298fbec114c72fbf5f781c6415584
Logon PF Fiap <logonpflocal@fiap.com.br>
2018-08-03 22:39:37 -0300

    C# Hello World

---
+++ 01.Fiap.HelloWorld/01.Fiap.HelloWorld.UI/Model/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.UI.Model
+{
+    class Pessoa
+    {
+        //Campos (atributos)
+        private string _nome;
+
+        //Propriedades (gets e sets)
+        public float Peso { get; set; }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _nome = value;
+                }
+            }
+        }
+    }
+}
